c7862af94358ce3209ab8e05cf31a1ac83a24087
Laura Hope <127255399+lauramhope@users.noreply.github.com>
2023-05-22 14:06:48 -0700

    Initial commit

---
+++ ToDoList/Migrations/20230522205426_AddItemsCompleted.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoList.Migrations
+{
+    public partial class AddItemsCompleted : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Completed",
+                table: "Items",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Completed",
+                table: "Items");
+        }
+    }
+}
